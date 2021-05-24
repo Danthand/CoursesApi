5f526bea301dc56c969e9238bfd1deb57968acf6
Pablo Reis <reis.pl1997@gmail.com>
2021-05-24 20:06:18 -0300

    gitignore fixed

---
+++ CoursesApi/Models/Courses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursesApi.Models
+{
+    public class Courses
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+
+        public uint? StudentsPerClass { get; set; }
+        public CategoryEnum Category { get; set; } 
+    }
+}
